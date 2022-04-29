b4cf12e2c8f164d5e946c8ca2256875c003e4caf
spymb <spym1933@foxmail.com>
2022-04-29 23:34:32 +0800

    添加chart10

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -10,6 +10,7 @@ import {Chart6} from '../components/chart6';
 import {Chart7} from '../components/chart7';
 import {Chart8} from '../components/chart8';
 import {Chart9} from '../components/chart9';
+import {Chart10} from '../components/chart10';
 
 export const Home = () => {
   const year = new Date().getFullYear();
@@ -44,7 +45,7 @@ export const Home = () => {
             <div className="bordered row1 案发类型">
               <h2>案发类型统计</h2>
               <div className="charts">
-
+                <Chart10/>
               </div>
             </div>
             <div className="bordered row2 案发街道">
