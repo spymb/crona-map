a9c77a89b11a6712dc92638bfa5b39465f0c3b9f
spymb <spym1933@foxmail.com>
2022-04-29 20:32:45 +0800

    准备添加chart6

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,6 +6,7 @@ import {Chart2} from '../components/chart2';
 import {Chart3} from '../components/chart3';
 import {Chart4} from '../components/chart4';
 import {Chart5} from '../components/chart5';
+import {Chart6} from '../components/chart6';
 
 export const Home = () => {
   const year = new Date().getFullYear();
@@ -25,7 +26,9 @@ export const Home = () => {
           <section className="bordered section3">
             <Chart5/>
           </section>
-          <section className="bordered section4">4</section>
+          <section className="bordered section4">
+            <Chart6/>
+          </section>
           <section className="bordered section5">5</section>
         </main>
         <footer>
