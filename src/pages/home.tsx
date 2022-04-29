c6f6ee501973ecf9816c7d147163904f33531c6c
spymb <spym1933@foxmail.com>
2022-04-29 18:14:53 +0800

    抽离出create-echarts-options.ts并添加chart2

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './home.scss';
 import headerBg from '../images/header.png';
 import {Chart1} from '../components/chart1';
+import {Chart2} from '../components/chart2';
 
 export const Home = () => {
   return (
@@ -11,6 +12,7 @@ export const Home = () => {
         <main>
           <section className="section1">
             <Chart1/>
+            <Chart2/>
           </section>
           <section className="bordered section2">2</section>
           <section className="bordered section3">3</section>
