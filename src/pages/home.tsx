8f4d27f9f250264ae1dadcd1275a2096b2bc1f3d
spymb <spym1933@foxmail.com>
2022-04-29 20:16:13 +0800

    添加chart3和chart4

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,8 +3,11 @@ import './home.scss';
 import headerBg from '../images/header.png';
 import {Chart1} from '../components/chart1';
 import {Chart2} from '../components/chart2';
+import {Chart3} from '../components/chart3';
+import {Chart4} from '../components/chart4';
 
 export const Home = () => {
+  const year = new Date().getFullYear();
   return (
     <div>
       <div className="home">
@@ -14,11 +17,17 @@ export const Home = () => {
             <Chart1/>
             <Chart2/>
           </section>
-          <section className="bordered section2">2</section>
+          <section className="bordered section2">
+            <Chart3/>
+            <Chart4/>
+          </section>
           <section className="bordered section3">3</section>
           <section className="bordered section4">4</section>
           <section className="bordered section5">5</section>
         </main>
+        <footer>
+          &copy; SPYM {year}
+        </footer>
       </div>
     </div>
   );
