816b17c3ba8ccf8a597758cb29abebd33941df0b
spymb <spym1933@foxmail.com>
2022-04-29 16:18:47 +0800

    区域划分1

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,6 +7,13 @@ export const Home = () => {
     <div>
       <div className="home">
         <header style={{backgroundImage: `url(${headerBg})`}}/>
+        <main>
+          <section className="section1">1</section>
+          <section className="section2">2</section>
+          <section className="section3">3</section>
+          <section className="section4">4</section>
+          <section className="section5">5</section>
+        </main>
       </div>
     </div>
   );
